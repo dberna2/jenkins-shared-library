693fe24a27a746f20cf21b231e444a966d0c31d9
David Leonardo Bernal <leo.bernal1946@gmail.com>
2022-02-15 17:20:04 +0100

    Update helloWordFromScript.groovy

--- vars/helloWordFromScript.groovy
+++ vars/helloWordFromScript.groovy
@@ -2,7 +2,7 @@ def call(Map config = [:]) {
     def scriptcontents = libraryResource "com/dberna2/scripts/${config.env}/${config.name}"
     writeFile file: "${config.name}", text: scriptcontents
     sh "chmod a+x ./${config.name}"
-    if(config.env = "linux") {
+    if(config.env == "linux") {
         echo "Hello Tux!"
     }else {
         echo "Hello Windown!"
