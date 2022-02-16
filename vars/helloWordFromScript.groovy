4f5e515caac1c1ca68667ffe80c35eae53da99db
David Bernal <david.bernallandazabal@plexus.es>
2022-02-16 10:23:21 +0100

    Add loadResource function

--- vars/helloWordFromScript.groovy
+++ vars/helloWordFromScript.groovy
@@ -1,10 +1,17 @@
 def call(Map config = [:]) {
-    def scriptcontents = libraryResource "com/dberna2/scripts/${config.env}/${config.name}"
+
+    def scriptcontents = loadResource("${config.env}", "${config.name}")
+    
     writeFile file: "${config.name}", text: scriptcontents
     sh "chmod a+x ./${config.name}"
-    if(config.env == "linux") {
+}
+
+def loadResource(String env, String  fileName) {
+    if(env == 'linux') {
         echo "Hello Tux!"
+        return libraryResource "com/dberna2/scripts/linux/${config.name}.sh"
     }else {
         echo "Hello Windows!"
+        return libraryResource "com/dberna2/scripts/windows/${config.name}.bat"
     }
 }
