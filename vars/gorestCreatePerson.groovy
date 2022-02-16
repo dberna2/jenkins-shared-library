7cd54c030be8cf3541af28bda32ccd21fb62c64a
David Bernal <david.bernallandazabal@plexus.es>
2022-02-16 12:55:39 +0100

    Implement request gorest API

---
+++ vars/gorestCreatePerson.groovy
@@ -0,0 +1,14 @@
+def call(Map config=[:]) {
+    def rawBody = libraryResource 'com/dberna2/api/createPerson.json'
+    
+    def binding = [
+        name: "${config.name}",
+        gender: "${config.gender}",
+        email: "${config.email}",
+        status: "${config.status}"
+    ]
+
+    def render = renderTemplate(rawBody, binding)
+    
+    sh('curl -D- -u $JIRA_CREDENTIALS -X POST --data "'+ render +'" -H "Content-Type: application/json" $JIRA_URL/public/v2/users')
+}
