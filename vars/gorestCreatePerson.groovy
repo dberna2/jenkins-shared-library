a3f1fe99e3913677f1c9b17ee0062fcd9c5dcd8f
David Bernal <david.bernallandazabal@plexus.es>
2022-02-17 15:53:14 +0100

    Read http status code request

--- vars/gorestCreatePerson.groovy
+++ vars/gorestCreatePerson.groovy
@@ -11,9 +11,10 @@ def call(Map config=[:]) {
     node {
         withCredentials([string(credentialsId: 'PERSON_API_ACCESS_TOKEN', variable: 'API_TOKEN')]) {
             String url = "$PERSON_API_URL/public/v2/users"
+            String contentType = "Content-Type: application/json"
             String authorization = "Authorization: Bearer $API_TOKEN"
             def render = renderTemplate(rawBody, binding)
-            def response = sh(script: "curl -D- -X POST --data '${render}' -H "Content-Type: application/json" -H $authorization $url", returnStdout: true).trim()
+            def response = sh(script: "curl -D- -X POST --data '${render}' -H $contentType -H $authorization $url", returnStdout: true).trim()
             echo response  + "Salida"
         }
     }
