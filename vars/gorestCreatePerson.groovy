f69574fdcee997212ff060f008ae3d7ab3f42b37
David Bernal <david.bernallandazabal@plexus.es>
2022-02-17 10:06:26 +0100

    Delete set +x command

--- vars/gorestCreatePerson.groovy
+++ vars/gorestCreatePerson.groovy
@@ -13,9 +13,9 @@ def call(Map config=[:]) {
 
             def render = renderTemplate(rawBody, binding)
         
-            sh '''
+            sh """
                 curl -D- -X POST --data "${render}" -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" $PERSON_API_URL/public/v2/users
-            '''
+            """
         }
     }
 }
