d4d1cbe7ebf27729d1d6421e46a3dc9e184a5fa0
David Bernal <david.bernallandazabal@plexus.es>
2022-02-17 11:13:42 +0100

    Scape render data

--- vars/renderTemplate.groovy
+++ vars/renderTemplate.groovy
@@ -1,5 +1,6 @@
 def call(input, binding) {
     def engine = new groovy.text.GStringTemplateEngine()
     def template = engine.createTemplate(input).make(binding)
+    echo template.toString()
     return template.toString()
 }
