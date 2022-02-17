bb33fc101ab1acedd94014c8420d44f90960e04b
David Bernal <david.bernallandazabal@plexus.es>
2022-02-17 16:20:43 +0100

    Manage curl response

--- vars/gorestCreatePerson.groovy
+++ vars/gorestCreatePerson.groovy
@@ -11,21 +11,11 @@ def call(Map config=[:]) {
     node {
         withCredentials([string(credentialsId: 'PERSON_API_ACCESS_TOKEN', variable: 'API_TOKEN')]) {
             def render = renderTemplate(rawBody, binding)
-            
-            final def (String response, int code) =
-                sh(
-                    script: "curl -D- -X POST --data '${render}' -H \"Content-Type: application/json\" -H \"Authorization: Bearer $API_TOKEN\" $PERSON_API_URL/public/v2/users", 
-                    returnStdout: true
-                ).trim()
-                .tokenize("\n")
-
-            echo "HTTP response status code: $code"
-
-            if (code == 200) {
-                def json = new groovy.json.JsonSlurperClassic().parseText(response)
-                echo json["id"]
-            }
-
+            def response = sh(
+                script: "curl -D- -X POST --data '${render}' -H \"Content-Type: application/json\" -H \"Authorization: Bearer $API_TOKEN\" $PERSON_API_URL/public/v2/users", 
+                returnStdout: true
+            ).trim()
+            echo response["id"]  + "Salida"
             echo response  + "Salida"
         }
     }
