49b9ab8cfc80d5f5e27c1a8f10474c5a26c69a6a
David Bernal <david.bernallandazabal@plexus.es>
2022-02-17 14:17:11 +0100

    add binding.toString()

--- vars/gorestCreatePerson.groovy
+++ vars/gorestCreatePerson.groovy
@@ -1,21 +1,21 @@
 def call(Map config=[:]) {
     def rawBody = libraryResource 'com/dberna2/api/person/createPerson.json'
 
+    def binding = [
+        name: "${config.name}",
+        gender: "${config.gender}",
+        email: "${config.email}",
+        status: "${config.status}"
+    ]
+
     node {
-        withCredentials([string(credentialsId: 'PERSON_API_ACCESS_TOKEN', variable: 'TOKEN')]) {
-
-            def binding = [
-                name: "${config.name}",
-                gender: "${config.gender}",
-                email: "${config.email}",
-                status: "${config.status}"
-            ]
-         
+        withCredentials([string(credentialsId: 'PERSON_API_ACCESS_TOKEN', variable: 'API_TOKEN')]) {
+            String url = "$PERSON_API_URL/public/v2/users"
+            String contentType = 'Content-Type: application/json'
+            String authorization = "Authorization: Bearer $API_TOKEN"
             def render = renderTemplate(rawBody, binding)
-
-            sh """
-                curl -D- -X POST --data '${render}' -H \"Content-Type: application/json\" -H \"Authorization: Bearer $TOKEN\" $PERSON_API_URL/public/v2/users
-            """
+            def response = sh(script: "curl -D- -X POST --data '${render}' -H $contentType -H $authorization $url", returnStdout: true).trim()
+            echo response
         }
     }
 }
